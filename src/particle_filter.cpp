0d89081aa49dbfd9e3049cf33f1409c6ace28094
Miro <miroslavln@gmail.com>
2017-04-23 18:51:56 -0400

    Cleaned up the already finished to-dos

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -16,10 +16,6 @@
 using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-    // TODO: Set the number of particles. Initialize all particles to first position (based on estimates of
-    //   x, y, theta and their uncertainties from GPS) and all weights to 1.
-    // Add random Gaussian noise to each particle.
-    // NOTE: Consult particle_filter.h for more information about this method (and others in this file).
     num_particles = 100;
 
     default_random_engine gen;
@@ -41,7 +37,6 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-
     if (fabs(yaw_rate) < 1e-6)
         return;
 
